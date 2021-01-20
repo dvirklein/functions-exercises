432ff22bab3e4673b9c5dcfae2e7480fb85860a3
dvirklein <77276010+dvirklein@users.noreply.github.com>
2021-01-21 00:18:13 +0200

    here my answar
    finnshed

--- Exercises.js
+++ Exercises.js
@@ -1,34 +1,64 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
+  let a = "";
+  for (i = str.length - 1; i >= 0; i--) {
+    a += str[i];
+  }
+  return a;
 }
 
 //Question 2
 function allCombinations(str) {
   let combinations = [];
-  // your code here
 
-  //
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i; j < str.length; j++) {
+      combinations.push(str.slice(i, j + 1));
+    }
+  }
   return combinations;
 }
 
 //Question 3
 function allCaps(str) {
+  let sum = "";
   // your code here
-  return "";
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i - 1) === " " || i === 0) {
+      sum += str.charAt(i).toUpperCase();
+    } else {
+      sum += str.charAt(i);
+    }
+  }
+  return sum;
 }
 
 //Question 4
 function myPower(x, n) {
-  // your code here
-  return "";
+  let z = 1;
+  for (i = 0; i < n; i++) {
+    z *= x;
+  }
+  return z;
 }
 
 //Question 5
 function getFirstNotRepeating(str) {
-  // your code here
-  return "";
+  for (i = 0; i < str.length; i++) {
+    let z = true;
+    for (j = 0; j < str.length; j++) {
+      if (str[i] === str[j] && i !== j) {
+        z = true;
+        break;
+      } else if (str[i] !== str[j] && i !== j) {
+        z = false;
+      }
+    }
+    if (z === false) {
+      return str[i];
+    }
+  }
+  return "no kfiliot";
 }
 
 //Question 6 (Bonus)
@@ -40,6 +70,7 @@ function isPrefectNumber(num) {
 // *** Playground ***
 // Feel free to run and test your code here on your own
 
+//
 // *** End of Playground ***
 
 // Don't touch me :)
